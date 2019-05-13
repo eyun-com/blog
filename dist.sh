820b76088485757b6798ab0ca1eb89b1d8da1d78
- <->
2019-05-13 10:30:30 +0800

    master

--- dist.sh
+++ dist.sh
@@ -3,6 +3,10 @@
 _dirname=$(realpath $(cd "$(dirname "$0")"; pwd))
 
 cd $_dirname
+git add .
+branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' |awk -F' ' '{print $2}'`
+git commit -m $branch ;
+git pull origin $branch && git push origin $branch;
 
 if ! hash hexo 2>/dev/null; then
 yarn global add hexo-cli
